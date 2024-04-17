9510695b889a67bd5373f47e769873cafc8858f2
songjhaha <songjh96@foxmail.com>
2024-04-17 14:57:13 +0800

    support help doc

--- tyjuliacall/__init__.py
+++ tyjuliacall/__init__.py
@@ -5,3 +5,31 @@ setup()
 
 JV = _load_pyjulia_core().JV
 sys.meta_path.insert(0, JuliaFinder())  # type: ignore
+
+
+def _setup_help():
+    import pydoc
+
+    class Helper(object):
+        def __init__(self):
+            if isinstance(__builtins__, dict):
+                self.help = __builtins__['help']
+            else:
+                self.help = __builtins__.help
+
+        def __call__(self, obj):
+            if isinstance(obj, JV):
+                # get Julia documentation
+                jdoc_func = JuliaEvaluator["jdoc_func = Base.string ∘ Base.Docs.doc"]
+                return pydoc.pager(jdoc_func(obj))
+            else:
+                return self.help(obj)
+
+    if isinstance(__builtins__, dict):
+        __builtins__['help'] = Helper()
+    else:
+        __builtins__.help = Helper()
+
+
+_setup_help()
+del _setup_help
